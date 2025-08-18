88edcb1cb529642e5bca8288cc64beb4c219174e
Michele <michele5rosato@gmail.com>
2025-08-18 12:46:47 +0200

    Analytics e dependecies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,36 @@ import {
   Routes,
   Route,
   Navigate,
+  useLocation
 } from 'react-router-dom';
 import Home from './pages/Home';
 import AuthForm from './pages/AuthForm';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import { hotjar } from 'react-hotjar';
+
+ReactGA.initialize("G-501543623");
 
 function App() {
+  // Componente per tracciare pageview GA
+  function GAListener() {
+    const location = useLocation();
+    useEffect(() => {
+      ReactGA.send({ hitType: "pageview", page: location.pathname });
+    }, [location]);
+    return null;
+  }
+
+  // Inizializza Hotjar una volta al montaggio
+  useEffect(() => {
+    hotjar.initialize(6496051, 6); // 6 è la versione snippet standard Hotjar
+  }, []);
+
   return (
     <Router>
+      <GAListener />
       <Routes>
         <Route path="/" element={<AuthForm />} />
         <Route
